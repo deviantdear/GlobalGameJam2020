b79f2477b1b030cd3748b35469457d0493623c4f
Valarie Adams <deviantdear@gmail.com>
2020-02-01 00:16:46 -0700

    Added some base inventory scripts and inventory UI with prefabs
    going to add some projectiles next!

---
+++ Assets/Scripts/Inventory/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Projectile", menuName = "Projectile")]
+public class Projectile : ScriptableObject
+{
+    new public string name = "New Projectile";
+    public Sprite icon = null;
+    public bool isDefaultProjectile = false;
+
+    public virtual void Use()
+    {
+        //uses item, something happens
+
+        Debug.Log("Using " + name);
+    }
+
+    public void RemoveFromInventory()
+    {
+        Inventory.instance.Remove(this); //Calls the Remove method from inventory script
+    }
+}
