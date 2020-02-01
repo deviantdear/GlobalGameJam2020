7e5eae474f49e0ce5e4c8a079bba1c5de36a51c1
vprime <myself@vincentprime.com>
2020-02-01 00:40:52 -0700

    Repairs after merge

--- Assets/Scripts/Parts/LaunchedProjectile.cs
+++ Assets/Scripts/Parts/LaunchedProjectile.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class LaunchedProjectile : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
