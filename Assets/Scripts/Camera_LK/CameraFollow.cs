c7b13e057ff5d0ac2444e809d89932f6b92470c4
Lize <eliza.knipscher@gmail.com>
2020-02-01 14:06:07 -0700

    Added camera script and MainLevel
    Added simple CameraFollow script -- the camera lerps smoothly after a target object (the cannon). Could be modified to follow after a delay. Also added MainLevel scene with a basic grid of four conveyor belts to move between.

---
+++ Assets/Scripts/Camera_LK/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float followSpeed;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, followSpeed * Time.deltaTime);
+    }
+}
