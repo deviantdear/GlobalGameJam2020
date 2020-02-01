e878423ea2efdb84ed93b43e2d5408cabf996bbd
roger.s@visuallive.com <schoellgenroger@gmail.com>
2020-01-31 23:19:15 -0700

    Created a MovementTrigger.cs that waits for Rigidbodies to enter. When they do it will apply a force to it every frame. The MovementTrigger.cs turns on when the conveyor belt activates.

---
+++ Assets/MovementTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This object manages a trigger that will move rigidbodies that fall into it. 
+/// </summary>
+public class MovementTrigger : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector3 forceDirection = Vector3.right;
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Move any rigidbody that falls into the trigger.
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(forceDirection);
+        }
+    }
+
+}
