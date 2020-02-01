ec83e8da3ba5d163255555cfe659bbc30aa47928
vprime <myself@vincentprime.com>
2020-01-31 23:30:11 -0700

    Bunch of placeholder scripts

---
+++ Assets/Scripts/Actions/Launcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Launcher controller
+/// </summary>
+public class Launcher : MonoBehaviour
+{
+    public Projectile Projectile { get; set; }
+
+    public UnityAction onLoading;
+    public UnityAction onLoaded;
+
+    public UnityAction onUnloading;
+    public UnityAction onUnloaded;
+    
+    public UnityAction onFire;
+
+    public UnityAction onCooldownStart;
+    public UnityAction onCooldownEnd;
+
+    public enum State
+    {
+        unarmed,
+        loading,
+        loaded,
+        firing,
+        cooldown
+    }
+
+    State currentState = State.unarmed;
+    public State CurrentState { get => currentState; }
+
+
+    [SerializeField]
+    RotationControl upRotationControl;
+
+}
---
+++ Assets/Scripts/Input/RotationControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Input controls for the cannon
+/// </summary>
+public class RotationControl : MonoBehaviour
+{
+    /// <summary>
+    /// When true, if input axis is 1, then the cannon is at 1.
+    /// When false,if input axis is 1, the cannon will rotate up at speed * 1
+    /// </summary>
+    [SerializeField] bool absoluteAxis = true;
+
+    /// <summary>
+    /// Current rotation
+    /// </summary>
+    [SerializeField] float rotation = 0f;
+
+    /// <summary>
+    /// Current rotation
+    /// </summary>
+    public float Rotation { get => rotation; }
+
+    /// <summary>
+    /// Speed at which object is turned
+    /// </summary>
+    [SerializeField] float speed = 1f;
+
+    /// <summary>
+    /// Vertical input axis
+    /// </summary>
+    [SerializeField] string inputName = "Rotation Control";
+
+
+    // Gather the input
+    void Update()
+    {
+        float input = Input.GetAxis(inputName);
+        if (absoluteAxis)
+        {
+            rotation = input;
+            return;
+        }
+        rotation = input * speed * Time.deltaTime;
+    }
+}
