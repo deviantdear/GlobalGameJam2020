43227c8f58df89ad95b23f493441ad82dd057908
roger.s@visuallive.com <schoellgenroger@gmail.com>
2020-01-31 22:09:09 -0700

    Added rotating gears to SpinningGears_Roger.unity.

---
+++ Assets/Scripts/Movement/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private bool on = false;
+    [SerializeField]
+    private float rotationSpeed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (on)
+        {
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
