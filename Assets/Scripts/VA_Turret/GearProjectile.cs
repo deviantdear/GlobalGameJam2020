f69291550b55263c055f902f1fb478c6eaf1bbd2
vprime <myself@vincentprime.com>
2020-02-01 18:54:55 -0700

    Adding more interfaces for projectiles to use

---
+++ Assets/Scripts/VA_Turret/GearProjectile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearProjectile : NormalProjectile
+{
+    [SerializeField] float destroyOnCollision = 2f;
+    [SerializeField] Gear gearPrefab;
+
+    internal void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<IInstallable>()?.Install(gearPrefab);
+        Destroy(gameObject, destroyOnCollision);
+    }
+}
