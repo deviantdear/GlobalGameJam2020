599132cd2293ed9f297040a188f12877e48c4b8a
Lize <eliza.knipscher@gmail.com>
2020-02-02 12:31:54 -0700

    Added timer
    Timer and font for timer. Timer exists as a UI prefab with a Timer script on it. Example can be found in the Kitten scene in Scenes/dev.

---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float secondsPassed;
+
+    private void Start()
+    {
+        secondsPassed = 0;
+        StartCoroutine(CountUp());
+    }
+
+    private IEnumerator CountUp()
+    {
+        var ts = TimeSpan.FromSeconds(secondsPassed);
+        timerText.text = string.Format("{0:00}:{1:00}", ts.TotalMinutes, ts.Seconds);
+        yield return new WaitForSeconds(1);
+        secondsPassed++;
+        StartCoroutine(CountUp());
+    }
+}
