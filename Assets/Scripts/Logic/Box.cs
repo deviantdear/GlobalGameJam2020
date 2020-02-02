288551b4b7c246a9270857a17c5f654a41894111
roger.s@visuallive.com <schoellgenroger@gmail.com>
2020-02-01 19:55:19 -0700

    Added the cat head logic for the box.

--- Assets/Scripts/Logic/Box.cs
+++ Assets/Scripts/Logic/Box.cs
@@ -9,16 +9,49 @@ namespace Level.Mechanics
     /// </summary>
     public class Box : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        private bool facingUp = false;
+        private Animator animator = null;
 
+        [SerializeField]
+        private string facingUpTrigger = "Facing Up Enter";
+        [SerializeField]
+        private string facingUpExitTrigger = "Facing Up Exit";
+
+        [SerializeField]
+        private GameObject debugCube = null;
+
+        public bool FacingUp
+        {
+            get => facingUp;
+            set
+            {
+                if (facingUp != value)
+                {
+                    //Trigger an animation if available.
+                    if (animator != null)
+                    {
+                        string trigger = value ? facingUpTrigger : facingUpExitTrigger;
+                        animator.SetTrigger(trigger);
+                    }
+
+                    //Turn on the debug cube
+                    if (debugCube != null)
+                        debugCube.gameObject.SetActive(value);
+                }
+                facingUp = value;
+            }
+        }
+
+        private void Awake()
+        {
+            //Get Components
+            animator = GetComponent<Animator>();
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            FacingUp = Vector3.Angle(transform.up, Vector3.up) < 80;
         }
     }
 }
