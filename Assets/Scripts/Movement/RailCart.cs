b773890a888782d9dfd8a62877ec1d2763935412
Vincent Prime <vincent@manonthemoonstudio.com>
2020-02-02 17:05:32 -0700

    Adding basic rail and rail cart movement

--- Assets/Scripts/Movement/RailCart.cs
+++ Assets/Scripts/Movement/RailCart.cs
@@ -8,15 +8,77 @@ using UnityEngine;
 /// </summary>
 public class RailCart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Rail currentRail;
+    [SerializeField] float speed = 1f;
+    [SerializeField] int currentSection = 0;
+
+    private float sectionStartTime = 0f;
+    private float sectionLength = 0f;
+    private float direction = 1f;
+
+    bool leftToRight = true;
+    Vector3 left;
+    Vector3 right;
+
+    public void TravelToSection(int sectionIndex)
     {
-        
+
+        leftToRight = true;
+        // Set the index
+        currentSection = sectionIndex;
+
+        Rail.RailSection section = currentRail.sections[currentSection];
+        left = section.left.position;
+        right = section.right.position;
+        sectionLength = Vector3.Distance(section.left.position, section.right.position);
+        sectionStartTime = Time.time;
+
+        // Teleport to the left side of the frame
+        transform.position = section.left.position;
     }
 
-    // Update is called once per frame
+    void ReverseDirection()
+    {
+        leftToRight = !leftToRight;
+        if (leftToRight)
+        {
+            left = currentRail.sections[currentSection].left.position;
+            right = currentRail.sections[currentSection].right.position;
+        }
+        else
+        {
+            left = currentRail.sections[currentSection].right.position;
+            right = currentRail.sections[currentSection].left.position;
+        }
+        sectionStartTime = Time.time;
+    }
+
+    void Start()
+    {
+        // Spawn at the first section and start traveling
+        TravelToSection(0);
+    }
+
+    // Move to the target end position.
     void Update()
     {
-        
+
+        // Distance moved equals elapsed time times speed..
+        float distCovered = (Time.time - sectionStartTime) * speed;
+
+        // Fraction of journey completed equals current distance divided by total distance.
+        float fractionOfJourney = distCovered / sectionLength;
+
+        if (fractionOfJourney >= 0.9 && direction > 0)
+        {
+            ReverseDirection();
+        } 
+        else if(fractionOfJourney <= 0.1 && direction < 0)
+        {
+            ReverseDirection();
+        }
+
+        // Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(left, right, fractionOfJourney);
     }
 }
